95ed1e8001e8b7a86ce51cffceb137ccd783364e
Pedro Soares <pedrosoaresvl.tech@gmail.com>
2025-01-23 21:29:27 -0300

    Move list added

---
+++ src/main/java/com/devnewbie/gameslist/dto/ReplacementDTO.java
@@ -0,0 +1,32 @@
+package com.devnewbie.gameslist.dto;
+
+public class ReplacementDTO {
+
+	private Integer sourceIndex;
+	private Integer destinationIndex;
+
+	public ReplacementDTO(){
+		
+	}
+	
+	public ReplacementDTO(Integer sourceIndex, Integer destinationIndex) {
+		super();
+		this.sourceIndex = sourceIndex;
+		this.destinationIndex = destinationIndex;
+	}
+	
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+	public Integer getDestinationIndex() {
+		return destinationIndex;
+	}
+	public void setDestinationIndex(Integer destinationIndex) {
+		this.destinationIndex = destinationIndex;
+	}
+	
+	
+}
--- src/main/java/com/devnewbie/gameslist/services/GameListService.java
+++ src/main/java/com/devnewbie/gameslist/services/GameListService.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devnewbie.gameslist.dto.GameListDTO;
-import com.devnewbie.gameslist.dto.GameMinDTO;
 import com.devnewbie.gameslist.entities.GameList;
+import com.devnewbie.gameslist.projections.GameMinProjection;
 import com.devnewbie.gameslist.repositories.GameListRepository;
+import com.devnewbie.gameslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -17,9 +18,28 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	@Transactional
+	public void move(Long listId, int soucerIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(soucerIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = soucerIndex < destinationIndex ? soucerIndex : destinationIndex;
+		int max = soucerIndex < destinationIndex ? destinationIndex : soucerIndex;	
+		
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
